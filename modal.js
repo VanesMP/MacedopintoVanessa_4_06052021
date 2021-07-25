573fc291b68a2279cb5b0c8228eed9126e4e20a5
VanesMP <vanessa.MP@outlook.fr>
2021-07-25 10:55:30 +0200

    envoie du formulaire quand c'est  valides

--- modal.js
+++ modal.js
@@ -239,7 +239,7 @@ function closeMessagCondition() {
 //2: pour ne pas executé deux fois TOUT le code de la methode vu que le resultat est deja recuperable au premier appel.
 
 function validate() {
-    event.preventDefault();
+
     var resultFirst = checkValidityFirst();
     var resultLast = checkValidityLast();
     var resultMail = checkValidityMail();
@@ -250,6 +250,7 @@ function validate() {
     if (resultFirst === true && resultLast === true && resultMail === true && resultBirthDate === true && resultQuantity === true && resultRadio === true && resultCondition === true) {
         alert("Merci! Votre inscription a été envoyée.");
     } else {
+        event.preventDefault();
         alert(" Le message n'a pas été envoyé car vous n'avez probablement pas saisis correctement tout les champs !");
 
     }
